626ecebdc3aadfd0a1a7f88bd60030c4c791eeba
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-06-09 15:39:08 +0300

    first attempt

--- js/cart.js
+++ js/cart.js
@@ -19,15 +19,28 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  
+  // deleteRow(table);
+  
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+  
 
   // TODO: Iterate over the items in the cart
+
   // TODO: Create a TR
+
+  let row = document.createElement('tr');
+  table.appendChild(row);
+
+
+
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
